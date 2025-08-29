9d8a70759bde911ab75ed8342efee97a679ff49b
hyeryun You <152360502+hyeryunYou@users.noreply.github.com>
2025-08-30 00:13:04 +0900

    feature: [#1] 공통 Button 컴포넌트 구현
    * feature: [#1] 공통 버튼 컴포넌트 구현
    
    * feature: [#1] 공통 버튼 컴포넌트 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,29 @@
 // 버튼 컴포넌트
+
+import styled from 'styled-components';
+
+const FooterBtn = styled.button`
+  display: flex;
+  width: 100%;
+  height: clamp(48px, 6vw, 56px);
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Pretendard-SemiBold';
+  font-size: clamp(14px, 2vw, 18px);
+  color: #FFFFFF;
+  background-color: #111111;
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
+`;
+
+export const Button = ({ text, onClick }) => {
+  return (
+    <FooterBtn
+      onClick={onClick}
+    >
+      {text}
+    </FooterBtn>
+  );
+};
