d17c80d2c2343ac4717f0b1c509fa320d39f5a4b
hyeryunYou <hyeryun1108@naver.com>
2025-09-04 23:12:32 +0900

    feature: 미디어쿼리 반응형 수정, 홈화면 배경 추가

--- src/styles/MainContainer.style.js
+++ src/styles/MainContainer.style.js
@@ -7,22 +7,4 @@ export const MainContainer = styled.div`
     justify-content: space-between;
     align-items: center; 
     box-sizing: border-box;
-
-    /* 기본: Galaxy Z Fold 5 (344px) */
-    width: min(344px, 100%);
-
-    /* Android 대표값 (360px) 이상 */
-    @media (min-width: 360px) {
-       width: min(360px, 100%);
-    }
-
-    /* iPhone SE/6/7/8, X/11 Pro 등 (375px) 이상 */
-    @media (min-width: 375px) {
-        width: min(375px, 100%);
-    }
-
-    /* iPhone 12/13/14 (390px) 이상 */
-    @media (min-width: 390px) {
-        width: min(390px, 100%);
-    }
 `;
